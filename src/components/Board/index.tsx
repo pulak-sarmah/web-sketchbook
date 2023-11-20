04ee335a801a76ca2fa8b74e2b21e7361a9e524c
pulak-sarmah <pulaksarmah333@gmail.com>
2023-11-20 11:53:03 +0530

    implemented drawing

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,11 +1,12 @@
 import { RootState } from "@/types";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const Board = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const { activeMenuItem, actionMenuItem } = useSelector(
-    (state: RootState) => state.menu
+  const shouldDraw = useRef<boolean>(false);
+  const activeMenuItem = useSelector(
+    (state: RootState) => state.menu.activeMenuItem
   );
   const { color, size } = useSelector(
     (state: RootState) => state.toolbox[activeMenuItem]
@@ -13,14 +14,59 @@ const Board = () => {
 
   useEffect(() => {
     if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
 
+    const changeConfig = () => {
+      context!.strokeStyle = color;
+      context!.lineWidth = size;
+    };
+
+    changeConfig();
+  }, [color, size]);
+
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const beginPath = (x: number, y: number) => {
+      context!.beginPath();
+      context!.moveTo(x, y);
+    };
+    const drawLine = (x: number, y: number) => {
+      context!.lineTo(x, y);
+      context!.stroke();
+    };
+    const handleMouseDown = (e: MouseEvent) => {
+      shouldDraw.current = true;
+      beginPath(e.clientX, e.clientY);
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!shouldDraw.current) return;
+      drawLine(e.clientX, e.clientY);
+    };
+
+    const handleMouseUp = (e: MouseEvent) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
   console.log(color, size);
+
   return <canvas ref={canvasRef}></canvas>;
 };
 
