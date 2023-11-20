1d72d0e80a2061f88fdbe6b255e124655dc4bc85
pulak-sarmah <pulaksarmah333@gmail.com>
2023-11-20 13:11:54 +0530

    implemented ability to download

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,16 +1,47 @@
+import { MENU_ITEMS } from "@/constant";
 import { RootState } from "@/types";
 import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
+import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
 const Board = () => {
+  const dispatch = useDispatch();
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const shouldDraw = useRef<boolean>(false);
-  const activeMenuItem = useSelector(
-    (state: RootState) => state.menu.activeMenuItem
+  const { activeMenuItem, actionMenuItem } = useSelector(
+    (state: RootState) => state.menu
   );
   const { color, size } = useSelector(
     (state: RootState) => state.toolbox[activeMenuItem]
   );
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
+      const existingContent = new Image();
+      existingContent.src = canvas.toDataURL();
+
+      context!.fillStyle = "#fff";
+      context!.fillRect(0, 0, canvas.width, canvas.height);
+
+      existingContent.onload = () => {
+        context!.drawImage(existingContent, 0, 0);
+
+        const URL = canvas.toDataURL();
+        const anchor = document.createElement("a");
+        anchor.href = URL;
+        anchor.download = "sketch.jpg";
+        anchor.click();
+
+        context!.fillStyle = "#fff";
+        context!.fillRect(0, 0, canvas.width, canvas.height);
+
+        dispatch(actionItemClick(null));
+      };
+    }
+  }, [actionMenuItem, color, dispatch, size]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -32,6 +63,7 @@ const Board = () => {
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    canvas.style.backgroundColor = "#fff";
 
     const beginPath = (x: number, y: number) => {
       context!.beginPath();
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,6 +25,10 @@ const Menu = () => {
     dispatch(menuItemClick(itemName));
   };
 
+  const handleActionItemClick = (itemName: string) => {
+    dispatch(actionItemClick(itemName));
+  };
+
   return (
     <div className={styles.menuContainer}>
       <div
@@ -43,13 +47,22 @@ const Menu = () => {
       >
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}
+      >
         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}
+      >
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}
+      >
         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
       </div>
     </div>
