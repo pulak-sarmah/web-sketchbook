cffae5fbd13222598927f1dd2a4da139f4b39260
pulak-sarmah <pulaksarmah333@gmail.com>
2023-11-21 10:43:31 +0530

    added production server

--- src/socket.ts
+++ src/socket.ts
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:8001");
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://web-sketchbook-server.onrender.com"
+    : "http://localhost:8001";
+export const socket = io(URL);
